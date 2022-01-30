278128ecf424feffba14082c599cbd422673d6ea
CMSuisse <cmarti@gmx.net>
2022-01-30 22:33:23 +0100

    Changed how files are structured. Balls now also respawn (but only 3 times)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 from ball import Ball
-from GameController import GameController
 
 pygame.init()
 screen = pygame.display.set_mode([640,640])
@@ -15,6 +14,26 @@ current_ball = Ball()
 #It shouldn't be able to just jump to the cursor. It should move quite slowly
 #Add a point system that adds a point when the ball touches the top of the screen
 #Make it viuslly obvious, how many points the player has and also how many balls it has left
+
+
+class GameController():
+    @staticmethod
+    def doUpdate():
+        global current_ball,balls_remaining
+        screen.fill((200,200,200)) #Start with a freshly filled in screen
+
+        if current_ball.isAlive:
+            current_ball.move()
+        else:
+            pygame.sprite.Sprite.kill(current_ball)
+            if balls_remaining > 0: #Has the player balls left?
+                current_ball = Ball() #If yes, spawn a new one
+                balls_remaining -= 1
+            else: #If no, it's game over
+                pass
+                #Game Over goes here
+
+        screen.blit(current_ball.image,current_ball.rect)
 
 active = True
 while active:
