241b5b0f9bbc523356e13300b7ce6441d0c73869
CMSuisse <cmarti@gmx.net>
2022-02-01 09:40:17 +0100

    Debugged the platform class and added some spicy new features...

--- main.py
+++ main.py
@@ -1,53 +1,69 @@
 from os import stat
 import pygame
 from ball import Ball
-from platform import Platform
+from platform_class import Platform
 
 pygame.init()
 screen = pygame.display.set_mode([640,640])
 screen.fill((200,200,200))
+pygame.display.set_caption("Pong")
 Clock = pygame.time.Clock()
 
 balls_remaining = 3
-current_ball = Ball()
+current_ball = pygame.sprite.Group()
+current_ball.add(Ball())
 platform = Platform()
 
-#Add a new pygame.sprite.Sprite class: platform
-#Give it the ability to follow a player's cursor's x-positon
-#It shouldn't be able to just jump to the cursor. It should move quite slowly
 #Add a point system that adds a point when the ball touches the top of the screen
 #Make it viuslly obvious, how many points the player has and also how many balls it has left
+#Trail renderer for the path the ball took. It should slowly grow smaller and dimmer.
+#The ball should speed up as the game progresses
 
 
 class GameController():
     @staticmethod
-    def __ballUpdate() -> None:
-        global current_ball,balls_remaining
-        if current_ball.isAlive:
-            current_ball.move()
-        else:
-            pygame.sprite.Sprite.kill(current_ball)
-            if balls_remaining > 0: #Has the player balls left?
-                current_ball = Ball() #If yes, spawn a new one
-                balls_remaining -= 1
-            else: #If no, it's game over
-                pass
-                #Game Over goes here
-
-    @staticmethod
     def doUpdate() -> None:
         screen.fill((200,200,200)) #Start with a freshly filled in screen
         GameController.__ballUpdate()
+        GameController.__platformUpdate()
+        GameController.__doCollisonLogic()
 
-        screen.blit(current_ball.image,current_ball.rect)
-
+        current_ball.draw(screen)
+        screen.blit(platform.image,platform.rect)
 
     @staticmethod
-    def getMouseX() -> int:
+    def __ballUpdate() -> None:
+        global current_ball,balls_remaining
+        for ball in current_ball:
+            if ball.isAlive:
+                ball.move()
+            else:
+                pygame.sprite.Sprite.kill(ball)
+                if balls_remaining > 0: #Has the player balls left?
+                    current_ball.add(Ball()) #If yes, spawn a new one
+                    balls_remaining -= 1
+                else: #If no, it's game over
+                    pass
+                    #Game Over goes here
+
+    @staticmethod
+    def __platformUpdate() -> None:
+        global platform
+        platform.move(GameController.__getMouseX())
+
+    @staticmethod
+    def __getMouseX() -> int:
         if pygame.mouse.get_focused():
             return pygame.mouse.get_pos()[0] #If the mouse is inside the window -> return the x-position of the mouse
 
-GameController.__ballUpdate()
+    @staticmethod
+    def __doCollisonLogic() -> None:
+        global current_ball, platform
+
+        if pygame.sprite.spritecollideany(platform,current_ball):
+            for ball in current_ball:
+                ball.speed[1] = -ball.speed[1]
+
 
 active = True
 while active:
@@ -57,7 +73,6 @@ while active:
 
     Clock.tick(60)
     GameController.doUpdate()
-    print(GameController.getMouseX())
     pygame.display.flip()
 
 pygame.quit()
