721abfe3637ee3cf0a742ca157c11c44c1223739
CMSuisse <cmarti@gmx.net>
2022-02-02 11:39:30 +0100

    Final push

---
+++ README.txt
@@ -0,0 +1,7 @@
+Pong v.1.0.0 by Cyrill Marti
+
+Background music:
+	- Original name: Chiptronical
+	- https://patrickdearteaga.com/arcade-music/
+
+All other sounds are Windows XP sound effects
--- main.py
+++ main.py
@@ -2,13 +2,23 @@ import pygame
 from ball import Ball
 from platform_class import Platform
 from trail_ball import TrailBall
-from game_over import game_over
+from game_over import game_over #That isn't confusing at all
 
-pygame.init()
+pygame.init() #Initializes both the font renderer and the mixer
 screen = pygame.display.set_mode([640,640])
 screen.fill((200,200,200))
 pygame.display.set_caption("Pong")
 Clock = pygame.time.Clock()
+
+#Initializing sound files for later use
+Background = pygame.mixer.Sound("Sounds/Background.ogg")
+Background.set_volume(0.2)
+PointGained = pygame.mixer.Sound("Sounds/PointGained.wav")
+PointGained.set_volume(0.7)
+NewBall = pygame.mixer.Sound("Sounds/NewBall.wav")
+NewBall.set_volume(1)
+YouLost = pygame.mixer.Sound("Sounds/YouLost.wav")
+YouLost.set_volume(1)
 
 balls_remaining = 3
 points = 0
@@ -26,7 +36,7 @@ class GameController():
 
         screen.fill((200,200,200)) #Start with a freshly filled in screen
         GameController.__ballUpdate()
-        GameController.__updateTrail()
+        GameController.updateTrail()
         GameController.__platformUpdate()
         GameController.__doCollisonLogic()
 
@@ -42,19 +52,23 @@ class GameController():
         global current_ball,balls_remaining,points,isGameOver
         for ball in current_ball:
             if ball.isAlive:
-                if ball.move() == 1:
+                if ball.move() == 1: #The function will return 1 when the ball hits the ceiling
+                    PointGained.play()
                     points += 1
                 ball.framesAlive += 1
             else:
                 pygame.sprite.Sprite.kill(ball)
                 balls_remaining -= 1
                 if balls_remaining > 0: #Has the player balls left?
+                    NewBall.play()
                     current_ball.add(Ball()) #If yes, spawn a new one
                 else: #If no, it's game over
+                    Background.stop()
+                    YouLost.play()
                     isGameOver = True
 
     @staticmethod
-    def __updateTrail():
+    def updateTrail():
         global current_ball,trail
         for ball in current_ball:
             trail.add(TrailBall((ball.rect.left,ball.rect.top),1))
@@ -84,16 +98,19 @@ class GameController():
                 ball.speed[1] = -ball.speed[1]
 
 
+Background.play(-1) #Special value -1 will loop the sound indefinitely
 active = True
 while active:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             active = False
+    Clock.tick(60)
     if not isGameOver:
-        Clock.tick(60)
         GameController.doUpdate()
     else:
         game_over(points,screen)
+        GameController.updateTrail()
+        trail.draw(screen)
     pygame.display.flip()
 
 pygame.quit()
